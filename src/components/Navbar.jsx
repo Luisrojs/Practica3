bd7d1349722689130987eaeeb2253d79b46c875b
Luisrojs <lrojs20014@gmail.com>
2025-05-04 21:15:53 +0200

    ajuste del navbar version sm responsive en proceso

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,8 +14,13 @@ export const Navbar = () => {
   
 
   return (
-    <nav class="">
-      <ul className={`bg-white py-[39px] w-[329px] h-[305px] absolute  left-0 right-0 mx-auto top-[106px] flex-col items-center  place-content-around after:content-[''] after:absolute after:top-[-24px] after:right-0 after:border-[12px] after:border-transparent after:border-b-white after:border-r-white text-DarkGrayishBlue text-xl ${menuClicked ? 'flex' : 'hidden'}`}>
+    <nav class="bg-green-700 w-full">
+      <ul className={`${menuClicked ? 'flex' : 'hidden'} bg-white py-[39px] w-[329px] h-[305px] absolute  left-0 right-0 mx-auto top-[106px] flex-col items-center  place-content-around after:content-[''] after:absolute after:top-[-24px] after:right-0 after:border-[12px] after:border-transparent after:border-b-white after:border-r-white text-DarkGrayishBlue text-xl 
+
+      /* desktop */
+
+      sm:bg-white
+      sm:flex sm:after:hidden sm:bg-transparent sm:flex-row sm:top-0 sm:w-[400px] sm:mx-0 sm:right-0 sm:h-[12px] sm:relative `}>
 
         <li class="cursor-pointer bg-Myyellow w-[140px] h-[56px] grid place-content-center rounded-full">
           About
@@ -26,7 +31,7 @@ export const Navbar = () => {
           Contact
         </li>
       </ul>
-      <div className="cursor-pointer" onClick={handleClick}>
+      <div className="cursor-pointer sm:hidden" onClick={handleClick}>
         <img src={burguerMenu} alt="menu de hamburguesa" />
       </div>
     </nav>
