116cacca0419bf2741e0cd481a28dff9c0eaa4ad
Luisrojs <lrojs20014@gmail.com>
2025-05-15 00:53:40 +0200

    creacion de services

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,11 @@ import { Navbar } from "./Navbar";
 
 export const Header = () => {
   return (
-    <header class="absolute bg-red-500 flex place-content-between w-full px-6 pt-8 font-Barlow font-bold">
-      <img src={logo} alt="Logo" />
-      <Navbar />
+    <header class=" absolute w-full px-6 pt-8  font-Barlow">
+      <div class="flex place-content-between">
+        <img src={logo} alt="Logo" />
+        <Navbar />
+      </div>
     </header>
   );
 };
